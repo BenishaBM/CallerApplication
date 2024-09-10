c54e644eca0aee09c157f89b0d7d773d929a3729
benisha <benisha.b@annulartechnologies.com>
2024-09-10 11:45:18 +0530

    testing

--- CallerApplication/src/main/java/com/annular/callerApplication/Util/Utility.java
+++ CallerApplication/src/main/java/com/annular/callerApplication/Util/Utility.java
@@ -18,6 +18,7 @@ public class Utility {
 
     public static String getSiteUrl(HttpServletRequest httpServletRequest) {
         String siteUrl = httpServletRequest.getRequestURL().toString();
+     
         return siteUrl.replace(httpServletRequest.getServletPath(), "");
     }
 
